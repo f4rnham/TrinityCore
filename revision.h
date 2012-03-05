684e0ab5c34a50d1a800e799ca03948f247b20ee
f4rnham <martin10225@gmail.com>
2012-03-05 23:00:54 +0100

    broken build, VS2010 and ...

--- revision.h
+++ revision.h
@@ -3,7 +3,7 @@
  #define _HASH                      "Archived"
  #define _DATE                      "0000-00-00 00:00:00 +0000"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2011 TrinityCore"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2012 TrinityCore"
  #define VER_FILEVERSION            0,0,0
  #define VER_FILEVERSION_STR        "0000-00-00 00:00:00 +0000 (Archived)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
